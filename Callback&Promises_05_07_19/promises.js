f79f20991260002387ec2af688166b94e6bf7ed0
AniketSoni-Webdunia <aniket.soni.sbj@gmail.com>
2019-07-07 23:35:04 +0530

    updation of prompt : promises task

--- Callback&Promises_05_07_19/promises.js
+++ Callback&Promises_05_07_19/promises.js
@@ -1,12 +1,15 @@
-//link : https://jsfiddle.net/r4wt3y8f/1/
+// link : https://jsfiddle.net/za7L0ox5/5/
+
+
 
 var login = (username,password)=>{
     return new Promise((resolve,reject)=>{
         console.log("logging you ...")
         setTimeout(()=>{
             if(username == "aniket" && password == "ani"){
-                var data={status:"success",token:"211ddcdsh1564"}
-                console.log("logged in!!!")
+                var data={status:"success",token:"211ddcdsh156"}
+                console.log("logged in!!!");
+                alert('logged in !!!')
                 resolve (data);           
             }else{
                 var error={status:"error",message:"Authentication Error",errorCode:404}
@@ -46,6 +49,7 @@ var getProfileDetails=(token)=>{
 }
 
  var ForgettenPassword=(err)=>{
+    alert(err.message);
     console.log(err.message);
     console.log("Getting help for you from our server")
     return new Promise((resolve,reject)=>{
@@ -57,13 +61,17 @@ var getProfileDetails=(token)=>{
 
 
 
+var username =prompt("Enter Username:","aniket");
+var password = prompt("Enter Password")
 
-
-login("aniket","ani") // login request(1)
+login(username,password) // login request(1)
 .then(data=>getProfileDetails(data.token)) // if request(1) resolve make request(2)
-.then(data=>console.log("hi,"+data.Fullname)) // if request(2) resolve print data
+.then(data=>alert("hi,"+data.Fullname)) // if request(2) resolve print data
 .catch(error=>{
     if(error.errorCode==404){
-       return ForgettenPassword(error).then(data=>console.log(data)).catch(error=>console.log(error))
+       return ForgettenPassword(error).then(data=>alert(data)).catch(error=>alert(error))
+    }else{
+        alert(error.message);
+        console.log(error.message);
     }
 }); 
